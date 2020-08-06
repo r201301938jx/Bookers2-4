34ca8b5a6aaffbfae9f3e58f9f6dc80b98e5c969
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-08-06 14:29:43 +0900

    [Finish]AdvancedWork3 Favorite Comment

--- Bookers2-4/config/routes.rb
+++ Bookers2-4/config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-  
+  resources :books, except: [:new] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+
   root 'home#top'
   get 'home/about'
   
